59eb1b876fbdca5c4c0a32923167bb9d049f31cd
Ria Ambadan <ria152130@gmail.com>
2024-08-23 22:09:16 +0530

    financial-aid-form added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Login';
 import SignupPage from './Signup';
-
+import FinancialForm from './financial-aid-form/form'
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/financialform" element={<FinancialForm />} />
       </Routes>
     </Router>
   );
