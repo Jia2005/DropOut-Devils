84c07524e1fab0d8d6764699431b1d536d0bf3f3
Shreya Bhatia <shreyakk17@gmail.com>
2024-08-24 11:09:51 +0530

    Handle the unauthorized and logout part

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { getAuth, signOut } from "firebase/auth";
+
+function signOutHome() {
+  const auth = getAuth();
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    alert('There are some server issues');
+  });
+}
 
 function HomePage() {
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+
   return (
-    <div>
+    user
+    ? <div>
       <h1>Welcome to the Home Page!</h1>
       <p>This is the landing page after a successful login.</p>
+      <button onClick={signOutHome}>Logout</button>
+    </div>
+    : <div>
+      Hello u are unauthorized
     </div>
   );
 }
