4483d565d352fd1a609ba2981711b0a16cf44923
Ria Ambadan <ria152130@gmail.com>
2024-08-24 11:27:57 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import Form from './Pages/financial-aid-form/form';
 import SignupPage from './Pages/Auth/Signup';
 import LoginPage from './Pages/Auth/Login';
 import HomePage from './Pages/Home/Home';
-<<<<<<< HEAD
 import TrackYourApplication from './Pages/financial-aid-form/trackYourApplication';
 import {app} from './firebase';
-=======
 import UnauthorizedPage from './Pages/Auth/Unauth';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { app } from './firebase';
+
 import Main from './main';
->>>>>>> 24a59cdcd7fe34fc2b4c9e5e84d1be4e1ae7a448
 
 function App() {
   return (
@@ -21,17 +18,11 @@ function App() {
 
         <Route path="/" element={<Main />} />
         <Route path="/form" element={<Form />} />
-<<<<<<< HEAD
         <Route path="/trackYourApplication" element={<TrackYourApplication />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-=======
         <Route path="/signup" element={<SignupPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/home" element={<HomePage/>}/>
         <Route path="/unauthorized" element={<UnauthorizedPage />} />
->>>>>>> 24a59cdcd7fe34fc2b4c9e5e84d1be4e1ae7a448
       </Routes>
     </Router>
   );
