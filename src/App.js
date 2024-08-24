73d5508aae80cfe29606129427b7e4d2d10dc034
Ria Ambadan <ria152130@gmail.com>
2024-08-24 11:50:36 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -13,12 +13,9 @@ function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/" element={<Main />} />
         <Route path="/form" element={<Form />} />
-
         <Route path="/trackYourApplication" element={<TrackYourApplication />} />
-
         <Route path="/signup" element={<SignupPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/home" element={<HomePage/>}/>
