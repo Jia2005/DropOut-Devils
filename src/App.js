9001d972319137e87712208e4d149c5b769043c6
Jia2005 <jia2.harisinghani@gmail.com>
2024-08-29 22:38:55 +0530

    Basic Update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import CreateQuizPage from './Pages/Quiz/Createquiz';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Form from './Pages/financial-aid-form/form';
-import SignupPage from './Pages/Auth/Signup';
 import LoginPage from './Pages/Auth/Login';
+import SignupPage from './Pages/Auth/Signup';
 import HomePage from './Pages/Home/Home';
 import QuizFormPage from './Pages/QuizStudent/QuizForm';
 import TrackYourApplication from './Pages/financial-aid-form/trackYourApplication';
@@ -37,8 +37,8 @@ function App() {
         <Route path="/trackYourApplication" element={<TrackYourApplication />} />
         <Route path="/Funds" element={<FundsDisbursement />} />
         <Route path="/applicationReview" element={<ApplicationReviewPage />} />
-        <Route path="/signup" element={<SignupPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
         <Route path="/home" element={<HomePage/>}/>
         <Route path="/quiz" element={<CreateQuizPage/>}/>
         <Route path="/studentquiz" element={<QuizFormPage/>}/>
