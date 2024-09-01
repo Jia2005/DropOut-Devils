3be6e5756dcdc2a500ad7435ea6c69a4460ae308
Jia2005 <jia2.harisinghani@gmail.com>
2024-09-01 10:26:38 +0530

    Extra routes removed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Form from './Pages/financial-aid-form/form';
 import LoginPage from './Pages/Auth/Login';
+import SignupPage from './Pages/Auth/Signup';
 import HomePage from './Pages/Home/Home';
 import { app } from './firebase';
 import Main from './main';
@@ -20,7 +21,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/form" element={<Form />} />
-        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/signup' element={<SignupPage />}/>
         <Route path='/adminsign' element={<AdminSign/>}/>
         <Route path='/profile' element={<UserProfile setComponent={setComponent}/>}/>
         <Route path="/home" element={<HomePage component={component} setComponent={setComponent}/>}/>
