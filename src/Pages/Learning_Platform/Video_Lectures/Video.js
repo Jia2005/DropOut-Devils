9ea6dd224cb5adc38b7ba641fb24225e794d0338
Shreya Bhatia <shreyakk17@gmail.com>
2024-09-17 22:05:43 +0530

    Changes landing page csss

--- src/Pages/Learning_Platform/Video_Lectures/Video.js
+++ src/Pages/Learning_Platform/Video_Lectures/Video.js
@@ -23,7 +23,7 @@ function VideoPlayer() {
   }, [videoId]);
 
   return (
-    <div style={{height:"100vh"}}>
+    <div style={{height:"98.9vh"}}>
       {videoUrl ? (
         <video width="100%" height="100%" controls>
           <source src={videoUrl} type="video/mp4" />
